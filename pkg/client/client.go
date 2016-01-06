4172c74b579b6e734a4a63cec7f36875e8658668
Ravi Sankar Penta <rpenta@redhat.com>
2016-01-06 11:30:18 -0800

    Add project networks interface

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -34,6 +34,7 @@ type Interface interface {
 	GroupsInterface
 	UserIdentityMappingsInterface
 	ProjectsInterface
+	ProjectNetworksInterface
 	ProjectRequestsInterface
 	LocalSubjectAccessReviewsImpersonator
 	SubjectAccessReviewsImpersonator
